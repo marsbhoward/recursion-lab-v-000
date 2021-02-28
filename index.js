fb39c9922462dee1b8b7f42e2abe8924cff37b9a
Mars Howard <marsbhoward@gmail.com>
2021-02-28 18:34:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function reverseString(string){
   console.log(string[0])
   if (string.length >1){
   let subString = string.substring(1,string.length);
+  console.log(subString)
   reverseString(subString);
   }
   else{
